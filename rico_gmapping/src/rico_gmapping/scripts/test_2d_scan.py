39f0272a5cc82d755766934f4e11cf83774db68e
rjia <rjia@accelrobotics.com>
2022-03-21 02:03:10 -0500

    test 2d scan init

--- rico_gmapping/src/rico_gmapping/scripts/test_2d_scan.py
+++ rico_gmapping/src/rico_gmapping/scripts/test_2d_scan.py
@@ -8,10 +8,19 @@ Theory:
 import rospy
 from sensor_msgs.msg import LaserScan
 
-
 if __name__ == '__main__':
     rospy.init_node("test_2d_scan", anonymous=True)     #anonymous=true ensures unique node name by adding random numbers
     # private namespace name 
     # pub = rospy.Publisher("~test_scan_before", LaserScan, latch=True, queue_size=1)
     pub = rospy.Publisher("test_scan_before", LaserScan, latch=True, queue_size=1)
+    msg = LaserScan()
+    msg.header.stamp = rospy.Time.now()
+    msg.header.frame_id = "base_scan"
+    msg.angle_min = -3.14
+    msg.angle_max = 3.14
+    msg.angle_increment = (msg.angle_max - msg.angle_min) / 360
+    msg.range_min = 0.0
+    msg.range_max = 3.0
+    msg.ranges.extend([0.9]* 360)
+    pub.publish(msg)
     rospy.spin()
