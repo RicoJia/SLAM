bb06e2175ba57261e6d4eee4cc8a2c3f91e28cc2
rjia <rjia@accelrobotics.com>
2022-03-26 14:44:49 -0500

    finished svd, goign to test

---
+++ rico_gmapping/src/rico_gmapping/include/rico_gmapping/icp_svd.hpp
@@ -0,0 +1,90 @@
+#ifndef __ICP_SVD_HPP__
+#define __ICP_SVD_HPP__
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+#include <Eigen/SVD>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <sophus/se3.hpp>
+#include <pcl/kdtree/kdtree_flann.h>
+/**
+* @brief: Use the SVD method to solve the iterative-closest-point problem, which solves for the affine transformation to align the current scan with the last scan
+* @ Notes: potential problems: 
+*   1. Association might be wrong. 
+* @param: last_scan
+* @param: current_scan
+* @return: transformation last scan -> current_scan
+*/
+#include <iostream> 
+using std::cout; using std::endl; 
+Eigen::Affine3d icp_svd(const std::vector<Eigen::Vector2d>& last_scan, const std::vector<Eigen::Vector2d>&current_scan, unsigned int num_iterations = 3){
+
+    // Build KD Tree with last_scan points [x, y] in base_link frame 
+    // there;s pcl::PointXYZ as well
+    pcl::PointCloud<pcl::PointXY>::Ptr cloud (new pcl::PointCloud<pcl::PointXY>);
+    for (const auto& s : last_scan){ 
+       cloud->push_back({s[0], s[1]}); 
+    }
+    pcl::KdTreeFLANN<pcl::PointXY>* kd_tree = new pcl::KdTreeFLANN<pcl::PointXY>(); 
+    kd_tree -> setInputCloud(cloud);
+    auto current_scan_cp = current_scan; 
+    // TODO
+    // Weird: auto R = Eigen::Matrix2d::Identity() auto will not be compatible with many. 
+    // So must write Eigen::Matrix2d
+    Eigen::Matrix2d R = Eigen::Matrix2d::Identity();
+    Eigen::Vector2d t = Eigen::Vector2d::Zero();
+    for (unsigned int i = 0; i < num_iterations; ++i) {
+        //TODO
+        // Find matched points
+        std::vector<Eigen::Vector2d> matched_last_scan;
+        matched_last_scan.reserve(last_scan.size());
+        for (const auto& s: current_scan){
+
+            // we just want 1 neighbor
+            int k = 1;
+            std::vector<int> index(1);
+            std::vector<float> squared_dist(1);
+            kd_tree->nearestKSearch({s[0], s[1]}, k, index, squared_dist);
+            matched_last_scan.emplace_back(last_scan.at(index[0]));
+
+            /* TODO:  <24-03-22, yourname> */
+            cout<<"======="<<endl;
+            cout<<last_scan.at(index[0])<<endl;
+            cout<<s<<endl;
+        }
+        
+        // Get miu
+        Eigen::Vector2d mu_x(0.0, 0.0);
+        Eigen::Vector2d mu_y(0.0, 0.0);
+        for (int i = 0; i < matched_last_scan.size(); ++i){
+            mu_x += matched_last_scan.at(i);
+            mu_y += current_scan.at(i);
+        }
+
+        mu_x /= mu_x.size();
+        mu_y /= mu_y.size();
+        // Get R, t.
+        Eigen::Matrix2d H;
+        for (int i = 0; i < matched_last_scan.size(); ++i){
+            H += (matched_last_scan.at(i) - mu_x) * ((current_scan.at(i) - mu_y).transpose());
+        }
+        Eigen::JacobiSVD<Eigen::Matrix2d> svd(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
+        // remember this is 2d
+        R = svd.matrixU() * (svd.matrixV().transpose());
+        t  = mu_x - R * mu_y;
+    }
+
+    delete kd_tree; 
+
+    Eigen::Affine3d transform = Eigen::Affine3d::Identity();
+    transform.translation() = Eigen::Vector3d(t[0], t[1], 0.0);
+    transform.linear().block<2,2>(0,0) = R; 
+    
+    return transform;
+}
+
+#endif /* end of include guard: __ICP_SVN_HPP__ */
--- rico_gmapping/src/rico_gmapping/tests/test_2d_scan_processor.cpp
+++ rico_gmapping/src/rico_gmapping/tests/test_2d_scan_processor.cpp
@@ -1,27 +1,50 @@
 #include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+// you need this for converting eigen to tf eigen 
+#include <tf_conversions/tf_eigen.h>
 #include <sensor_msgs/LaserScan.h>
 #include "rico_gmapping/rico_gmapping.hpp"
+#include "rico_gmapping/icp_svd.hpp"
 #include <vector>
+#include <iostream>
+#include <cmath>
+using std::cout; using std::endl; 
 
-/**
-* @brief: brief
-* @param: const 
-* @return: void
-*/
+std::vector<Eigen::Vector2d> last_scan; 
 
-sensor_msgs::LaserScan::ConstPtr map_scan; 
-void map_scan_cb(const sensor_msgs::LaserScan::ConstPtr& msg)
-{
-    map_scan = msg; 
+void build_valid_scan_vec(const sensor_msgs::LaserScan::ConstPtr& msg, std::vector<Eigen::Vector2d>& scan){
+    double angle = 0.0; 
+    for (const auto& s: msg->ranges){
+        if (msg->range_min < s && s < msg->range_max){
+           scan.emplace_back(s * std::cos(angle), s * std::sin(angle)) ; 
+        }
+        angle += msg -> angle_increment; 
+    } 
 }
 
-sensor_msgs::LaserScan::ConstPtr base_scan; 
-void base_scan_cb(const sensor_msgs::LaserScan::ConstPtr& msg)
+void map_scan_cb(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    base_scan = msg; 
-    if (map_scan){
+    build_valid_scan_vec(msg, last_scan); 
+}
+
+void base_link_scan_cb(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+    Eigen::Affine3d t_map_base_scan; 
+    if (!last_scan.empty()){ 
         ROS_INFO_STREAM("Received map scan, processing base scan.");
+
+        std::vector<Eigen::Vector2d> current_scan; 
+        build_valid_scan_vec(msg, current_scan); 
+
+        t_map_base_scan = icp_svd(last_scan, current_scan, 5);
     }
+
+    // publish tf 
+    static tf::TransformBroadcaster br; 
+    tf::Transform transform; 
+    tf::transformEigenToTF(t_map_base_scan, transform); 
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "base_scan"));
+    ROS_INFO_STREAM("Published TF map->base_scan"); 
 }
 
 int main(int argc, char**argv){
@@ -31,6 +54,6 @@ int main(int argc, char**argv){
     ROS_INFO_STREAM("Started "<<node_name); 
 
     auto map_scan_sub = nh.subscribe<sensor_msgs::LaserScan>("/map_scan", 1, map_scan_cb); 
-    auto base_scan_sub = nh.subscribe<sensor_msgs::LaserScan>("/base_link_scan", 1, base_scan_cb); 
+    auto base_scan_sub = nh.subscribe<sensor_msgs::LaserScan>("/base_link_scan", 1, base_link_scan_cb); 
     ros::spin(); 
 }
