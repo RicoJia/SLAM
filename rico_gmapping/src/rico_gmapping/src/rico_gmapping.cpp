5108b1808cc502e9203a54d2d10accff112335ef
rjia <rjia@accelrobotics.com>
2022-04-13 12:00:38 -0500

    modifying rico gmapping

--- rico_gmapping/src/rico_gmapping/src/rico_gmapping.cpp
+++ rico_gmapping/src/rico_gmapping/src/rico_gmapping.cpp
@@ -1,11 +1,14 @@
 #include <ros/ros.h>
-#include <nav_msgs/OccupancyGrid.h>
 #include "rico_gmapping/rico_gmapping.hpp"
 #include <vector>
 
 /**
+ * Theory
+ *  1. rviz error: Error subscribing: Character [ ] at element [18] is not valid in Graph Resource Name [/rico_gmapping/map ].  Valid characters are a-z, A-Z, 0-9, / and _., this means the map topic name you put into rviz is not correct, maybe have an extra space or something
+*/
+/**
 * @brief: random function to fill in map info
-* @param: nav_msgs::OccupancyGrid& 
+* @param: nav_msgs::OccupancyGrid&
 * @return: void
 */
 void fill_in_map(nav_msgs::OccupancyGrid& map)
@@ -18,34 +21,10 @@ void fill_in_map(nav_msgs::OccupancyGrid& map)
     map.info.origin.orientation.w = 1.0;
     map.info.map_load_time = ros::Time::now();
         map.info.origin.position.y = 0.0;
-    map.info.origin.position.x = 0.0; 
+    map.info.origin.position.x = 0.0;
     // really, this is an int8[], so just 1D array
     map.data.assign(map.info.height * map.info.width, 0);
 }
 
-/**
- * Theory
- *  1. rviz error: Error subscribing: Character [ ] at element [18] is not valid in Graph Resource Name [/rico_gmapping/map ].  Valid characters are a-z, A-Z, 0-9, / and _., this means the map topic name you put into rviz is not correct, maybe have an extra space or something
-*/
-// Params:
-// *  - @b "~map_update_interval": @b [double] time in seconds between two recalculations of the map
 
-int main(int argc, char**argv)
-{
-    /**
-    * @brief: ROS Node 
-    */
-    // node name is hello
-    ros::init(argc, argv, "rico_gmapping"); 
-    ros::NodeHandle nh("~"); 
-    // https://github.com/msc9533/ros-local-map-publisher/blob/master/src/local_map_builder/src/local_map.cpp
-    ros::Publisher map_pub = nh.advertise<nav_msgs::OccupancyGrid>("map", 1);
-    ros::Rate r(50);
-    nav_msgs::OccupancyGrid map;
-    while(ros::ok()){
-        fill_in_map(map);
-        map_pub.publish(map); 
-        ros::spinOnce();
-        r.sleep();
-    }
-}
+
